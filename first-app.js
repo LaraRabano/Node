857ef607b2294cf665002d33d021fdbfda620962
Lara R <lararabanoh@gmail.com>
2023-07-11 23:59:17 +0200

    feat(Node): new comments

--- first-app.js
+++ first-app.js
@@ -1,3 +1,4 @@
-const fs = require('fs');  //Así se escriben las constantes en JS (no me gusta, la verdad)
+const fs = require('fs');  //Así se importan módulos en JS
+//Así se escriben las constantes en JS (no me gusta, la verdad)
 
 fs.writeFileSync('hello.txt', 'Hello from Node.js'); //Esta es una manera extraña de JS de crear archivos con contenido (viva Python)
