a25522c9abceaf76d42e8710e667f149d8c875e8
Lara R <lararabanoh@gmail.com>
2023-07-11 20:40:59 +0200

    feat(Node): Creating docs

--- first-app.js
+++ first-app.js
@@ -1,1 +1,3 @@
-console.log("Hello from Node.js"); 
+const fs = require('fs');  //Así se escriben las constantes en JS (no me gusta, la verdad)
+
+fs.writeFileSync('hello.txt', 'Hello from Node.js'); //Esta es una manera extraña de JS de crear archivos con contenido (viva Python)
