fbd07e29c7dc7d554e5bca1d748c8202a36745c8
Lara R <lararabanoh@gmail.com>
2023-07-17 17:40:17 +0200

    feat(Node): Refreshing the Core Syntax

--- first-app.js
+++ first-app.js
@@ -1,4 +1,4 @@
 const fs = require('fs');  //Así se importan módulos en JS
 //Así se escriben las constantes en JS (no me gusta, la verdad)
 
-fs.writeFileSync('hello.txt', 'Hello from Node.js'); //Esta es una manera extraña de JS de crear archivos con contenido (viva Python)
+fs.writeFileSync('hello.txt', 'Hello from Node.js'); //Esta es una manera extraña de JS de crear archivos con contenido (viva Python)g
---
+++ play.js
@@ -0,0 +1,9 @@
+let userName = "Luis";
+let age = 28
+let hasHobbies = true
+
+function summarizeUser(userName, age, hasHobbies) {
+    return "Name is " + userName + ", age is " + age + " and " + userName + " has hobbies."
+} 
+
+console.log(summarizeUser(userName, age, hasHobbies));
