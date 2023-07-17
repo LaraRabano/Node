0c097dbc8811c67ecbdf6518effec5978245231d
Lara R <lararabanoh@gmail.com>
2023-07-17 18:05:33 +0200

    feat(Node): forma nº 1 de asignar funciones a una variable

--- play.js
+++ play.js
@@ -2,8 +2,9 @@ let userName = "Luis";
 let age = 28
 let hasHobbies = true
 
-function summarizeUser(userName, age, hasHobbies) {
+
+const summarizeUser = function(userName, age, hasHobbies) {
     return "Name is " + userName + ", age is " + age + " and " + userName + " has hobbies."
-} 
+} //Esto es una función anónima a la que se le asigna un nombre, cuando se asigna a la variable a través del const. 
 
 console.log(summarizeUser(userName, age, hasHobbies));
